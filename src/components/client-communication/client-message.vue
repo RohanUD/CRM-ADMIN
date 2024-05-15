98e9efe459a096bfa490e04d86d6c03ab8d25975
RohanUD <78956997+RohanUD@users.noreply.github.com>
2024-05-15 16:37:16 +0530

    Initial commit

---
+++ src/components/client-communication/client-message.vue
@@ -0,0 +1,33 @@
+<template>
+  <div :class="{'dark': backgroundChange}">
+    <h5>{{ title }}</h5>
+    {{ text }}
+  </div>
+</template>
+
+<script>
+export default {
+    props : {
+        text : String,
+        title : String,
+        backgroundChange : Boolean
+    }
+}
+</script>
+
+<style scoped>
+.message {
+  background: #e7e7e7;
+  border-radius: 10px;
+  padding: 1rem;
+  width: fit-content;
+}
+
+.message.dark {
+  background: #e9eaf6;
+}
+
+h5 {
+  margin: 0 0 .5rem 0;
+}
+</style>
